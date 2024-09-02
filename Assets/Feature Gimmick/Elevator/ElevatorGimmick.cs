fca1b83258a823d1f33958ab3b202da1e1d8a4fc
진성혁 <wtg0516@naver.com>
2024-09-02 18:49:59 +0900

    엘리베이터 기믹 에셋 변경, 불빛 단계별 수정

--- Assets/Feature Gimmick/Elevator/ElevatorGimmick.cs
+++ Assets/Feature Gimmick/Elevator/ElevatorGimmick.cs
@@ -20,11 +20,22 @@ public class ElevatorGimmick : Gimmick
     [SerializeField]
     private GameObject room;
 
+    [SerializeField]
+    private MeshRenderer ren;
+    private Material[] tempMatList;
+    [SerializeField]
+    private Material whiteLightMat;
+    [SerializeField]
+    private Material BlinkLightMat;
+    [SerializeField]
+    private Material BlackLightMat;
+
     private void Awake()
     {
         impulseSource = GetComponent<CinemachineImpulseSource>();
         tmo = GetComponentInChildren<TextMeshPro>();
         camShaker = GetComponent<CamShaker>();
+        tempMatList = ren.materials;
         gameObject.SetActive(false);
     }
 
@@ -47,10 +58,13 @@ public class ElevatorGimmick : Gimmick
         camShaker.StopShakeCam();
         floor = 6;
         tmo.text = "6";
-        room.SetActive(true);
+        //전등 다시 정상화
+        tempMatList[1] = whiteLightMat;
+        ren.materials = tempMatList;
         //엘리베이터 상승음 종료
         AudioManager.instance.StopSound(AudioManager.instance.elevatorMove, FMOD.Studio.STOP_MODE.IMMEDIATE);
         AudioManager.instance.StopSound(AudioManager.instance.elevatorFast, FMOD.Studio.STOP_MODE.IMMEDIATE);
+        room.SetActive(true);
         gameObject.SetActive(false);
     }
 
@@ -75,6 +89,9 @@ public class ElevatorGimmick : Gimmick
         AudioManager.instance.PlaySound(AudioManager.instance.elevatorCrash, transform.position);
         yield return new WaitForSeconds(1.7f);
         impulseSource.GenerateImpulseWithForce(3);
+        //충격과 동시에 전등에 이상생김
+        tempMatList[1] = BlinkLightMat;
+        ren.materials = tempMatList;
         AudioManager.instance.StopSound(AudioManager.instance.elevatorMove, FMOD.Studio.STOP_MODE.IMMEDIATE);
         tmo.text = "ERROR";
         yield return new WaitForSeconds(5);
@@ -92,6 +109,9 @@ public class ElevatorGimmick : Gimmick
         //쾅! 소리들리면서 카메라 흔들림
         AudioManager.instance.PlaySound(AudioManager.instance.hit, transform.position);
         impulseSource.GenerateImpulseWithForce(4);
+        //충격과 동시에 전등 꺼짐
+        tempMatList[1] = BlackLightMat;
+        ren.materials = tempMatList;
 
         //갑자기 급격히 상승
         AudioManager.instance.PlaySound(AudioManager.instance.elevatorFast, transform.position);
@@ -128,6 +148,7 @@ public class ElevatorGimmick : Gimmick
             if (PlayerConstant.EyeBlinkLAT >= 6)
             {
                 Deactivate();
+                yield break;
             }
             randomNum = Random.Range(100000, 1000000000);
             tmo.text = randomNum.ToString();
