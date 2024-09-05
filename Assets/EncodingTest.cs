49830508b891101c019e84f4b6658d79b354162f
진성혁 <wtg0516@naver.com>
2024-09-05 20:51:44 +0900

    인코딩 테스트(.editorconfig 파일 추가함)

---
+++ Assets/EncodingTest.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EncodingTest : MonoBehaviour
+{
+    /// <summary>
+    /// 새로만든 파일 주석
+    /// </summary>
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
