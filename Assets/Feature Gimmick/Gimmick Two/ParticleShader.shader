6f7ede055a86fb22c563d7423e557aeb27f45915
muryung <62589813+muryung@users.noreply.github.com>
2024-09-25 22:21:26 +0800

    파티클 생성

--- Assets/Feature Gimmick/Gimmick Two/ParticleShader.shader
+++ Assets/Feature Gimmick/Gimmick Two/ParticleShader.shader
@@ -1,3 +1,7 @@
+// Upgrade NOTE: replaced 'mul(UNITY_MATRIX_MVP,*)' with 'UnityObjectToClipPos(*)'
+
+// Upgrade NOTE: replaced '_Object2World' with 'unity_ObjectToWorld'
+
 Shader "Custom/ParticleShader"
 {
     Properties
@@ -13,27 +17,32 @@ Shader "Custom/ParticleShader"
             CGPROGRAM
             #pragma vertex vert;
             #pragma fragment frag;
+            #pragma multi_compile_instancing
             #pragma target 4.5
-
-            struct appdata
-            {
-                float4 pos : POSITION;
-            };
-
+            
             struct v2f
             {
                 float4 pos : POSITION;
                 float2 uv : TEXCOORD0;
             };
 
-            StructuredBuffer<float3> _Positions;
-
-            v2f vert(appdata v, uint id : SV_InstanceID)        // TODO: Add the instance ID
+            struct Particle
             {
-                float3 position = _Positions[id];
+                float3 position;
+            };
+
+            StructuredBuffer<Particle> _Particles;
+            float _ParticleRadius;
+
+            v2f vert(v2f v, uint id : SV_InstanceID)        // TODO: Add the instance ID
+            {
                 v2f o;
+                unity_ObjectToWorld._11_21_31_41 = float4(_ParticleRadius, 0, 0, 0);
+				unity_ObjectToWorld._12_22_32_42 = float4(0, _ParticleRadius, 0, 0);
+				unity_ObjectToWorld._13_23_33_43 = float4(0, 0, _ParticleRadius, 0);
+				unity_ObjectToWorld._14_24_34_44 = float4(_Particles[id].position.xyz, 1);
                 
-                o.pos = UnityObjectToClipPos(v.pos + float4(position, 0));
+                o.pos = UnityObjectToClipPos(v.pos);
                 return o;
             }
 
--- Assets/Feature Gimmick/Gimmick Two/ParticleSystem.cs
+++ Assets/Feature Gimmick/Gimmick Two/ParticleSystem.cs
@@ -1,46 +1,62 @@
-using System;
 using System.Collections.Generic;
-using Unity.Burst;
-using Unity.Collections;
-using Unity.Jobs;
+using System.Runtime.InteropServices;
 using Unity.Mathematics;
-using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.Jobs;
-using UnityEngine.Rendering;
-using UnityEngine.Serialization;
+using Random = UnityEngine.Random;
+
+
 namespace Bed.Gimmick
 {
     public class ParticleSystem : MonoBehaviour
     {
-        public int particleCount = 1000;
-        public float particleDiameter = 0.1f;
-        public Vector3 particlesSpawnBox = new Vector3(1, 1, 1);
-        public Vector3 boundingBox = new Vector3(1, 1, 1);
+        [StructLayout(LayoutKind.Sequential, Size = 12)]
+        struct Particle
+        {
+            public float3 position;
+        }
 
+        [Header("파티클 생성 옵션")]
+        public float particleRadius = 0.1f; // 파티클의 반지름
+        public Vector3 particleSpawnPoint;  // 파티클이 생성되는 위치
+        public Vector3Int particleSpawnBox; // 박스를 만들어서 그 안에 파티클이 생성되도록 함
+        public Vector3 particleBoundBox;    // 파티클을 가두는 박스
+
+        [Header("파티클 생성에 필요한 것")]
         [SerializeField] private Mesh particleMesh;
         [SerializeField] private Material particleMaterial;
-        
         [SerializeField] private ComputeShader computeShader;
-
-        private ComputeBuffer velocityBuffer;
-        private GraphicsBuffer graphicsBuffer;
-        private GraphicsBuffer positionBuffer;
-        private static int commandCount = 1;
         
-        private Vector3[] velocities;
+        private Particle[] particles;
+        private int particleCount => particleSpawnBox.x * particleSpawnBox.y * particleSpawnBox.z;
+        
+        private ComputeBuffer particleBuffer;
+        private GraphicsBuffer graphicsBuffer;
+        private const int commandCount = 1;
+
         private RenderParams renderParams;
         private GraphicsBuffer.IndirectDrawIndexedArgs[] commandData = new GraphicsBuffer.IndirectDrawIndexedArgs[commandCount];
-        
-        private static int positionPropertyID = Shader.PropertyToID("_Positions");
 
-        private float3 myPosition;
-        
+        private void Awake()
+        {
+            if (Vector3.Distance(particleSpawnBox, particleBoundBox) < 0)
+            {
+                Debug.LogError("Spawn box 보다 bouding Box가 더 커야합니다.");
+                return;
+            }
+        }
+
         private void OnEnable()
         {
-            velocityBuffer = new ComputeBuffer(particleCount, 3 * sizeof(float));
-            positionBuffer = new GraphicsBuffer(GraphicsBuffer.Target.Structured, 1, 3 * sizeof(float));
+            SpawnParticles();
+            particleBuffer = new ComputeBuffer(particleCount, Marshal.SizeOf(typeof(Particle)));
+            particleBuffer.SetData(particles);
+            
+            commandData[0].indexCountPerInstance = particleMesh.GetIndexCount(0);
+            commandData[0].startIndex = particleMesh.GetIndexStart(0);
+            commandData[0].baseVertexIndex = particleMesh.GetBaseVertex(0);
+            commandData[0].instanceCount = (uint) particleCount;
             graphicsBuffer = new GraphicsBuffer(GraphicsBuffer.Target.IndirectArguments, particleCount, GraphicsBuffer.IndirectDrawIndexedArgs.size);
+            graphicsBuffer.SetData(commandData);
         }
 
         private void Start()
@@ -49,55 +65,61 @@ namespace Bed.Gimmick
             {
                 material = particleMaterial,
                 matProps = new MaterialPropertyBlock(),
-                worldBounds = new Bounds(transform.position, boundingBox)
+                worldBounds = new Bounds(transform.position, particleBoundBox)
             };
-            positionBuffer.SetData(new Vector3[] {new Vector3(10,10,10)});
-            renderParams.matProps.SetBuffer(positionPropertyID, positionBuffer);
-            
-            commandData[0].indexCountPerInstance = particleMesh.GetIndexCount(0);
-            commandData[0].startIndex = particleMesh.GetIndexStart(0);
-            commandData[0].baseVertexIndex = particleMesh.GetBaseVertex(0);
-            commandData[0].instanceCount = 1;
-            graphicsBuffer.SetData(commandData);
-            if (Vector3.Distance(particlesSpawnBox, boundingBox) < 0)
-            {
-                Debug.LogError("Spawn box 보다 bouding Box가 더 커야합니다.");
-                return;
-            }
-            // velocities = new Vector3[particleCount];
-            // velocityBuffer.SetData(velocities);
-            // computeShader.SetBuffer(0, "_Velocity", velocityBuffer);
-            // computeShader.SetFloat("deltaTime", Time.deltaTime);
+            computeShader.SetBuffer(0, "_Particles", particleBuffer);
+
         }
 
         private void Update()
         {
             //computeShader.Dispatch(0, particleCount / 8, 1, 1);
+            particleMaterial.SetBuffer("_Particles", particleBuffer);
+            particleMaterial.SetFloat("_ParticleRadius", particleRadius);
             Graphics.RenderMeshIndirect(renderParams, particleMesh, graphicsBuffer, commandCount);
         }
 
-        private void SpawnParticles(GameObject particlePrefab)
+        private void SpawnParticles()
         {
-            for (int i = 0; i < particlesSpawnBox.x; i++)
-            for (int j = 0; j < particlesSpawnBox.y; j++)
-            for (int k = 0; k < particlesSpawnBox.z; k++)
+            float x, y, z;
+            List<Particle> particleList = new List<Particle>();
+            
+            // 박스의 중심을 다시 박스의 좌표를 빼서 박스의 시작점을 구함으로써 Center를 중심으로 좌우로 박스를 그리게 함
+            particleSpawnPoint = new Vector3()
             {
-                var tmpObj = Instantiate(particlePrefab, new Vector3(i * particleDiameter, j * particleDiameter, k * particleDiameter), Quaternion.identity);
+                x = particleSpawnPoint.x - (particleSpawnBox.x * particleRadius),
+                y = particleSpawnPoint.y - (particleSpawnBox.y * particleRadius),
+                z = particleSpawnPoint.z - (particleSpawnBox.z * particleRadius)
+            };
+            particleSpawnPoint += Random.onUnitSphere * particleRadius * 0.2f;
+            
+            for (int i = 0; i < particleSpawnBox.x; i++)
+            for (int j = 0; j < particleSpawnBox.y; j++)
+            for (int k = 0; k < particleSpawnBox.z; k++)
+            {
+                x = particleSpawnPoint.x + (i * particleRadius * 2f);
+                y = particleSpawnPoint.y + (j * particleRadius * 2f);
+                z = particleSpawnPoint.z + (k * particleRadius * 2f);
+                particleList.Add(new Particle()
+                {
+                    position = new Vector3(x, y, z)
+                });
             }
+            particles = particleList.ToArray();
         }
-
+        
         private void OnDrawGizmos()
         {
             Gizmos.color = Color.green;
-            Gizmos.DrawWireCube(transform.position, boundingBox);
+            Gizmos.DrawWireCube(transform.position, particleBoundBox);
         }
 
         private void OnDisable()
         {
-            velocityBuffer?.Release();
             graphicsBuffer?.Release();
-            velocityBuffer = null;
+            particleBuffer?.Release();
             graphicsBuffer = null;
+            particleBuffer = null;
         }
 
     }
