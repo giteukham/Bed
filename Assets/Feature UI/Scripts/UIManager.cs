4e32a3fb20ebc4b2c106e7675a9cb7f0f9c44c06
진성혁 <wtg0516@naver.com>
2024-11-03 15:18:08 +0900

    임시 커밋

--- Assets/Feature UI/Scripts/UIManager.cs
+++ Assets/Feature UI/Scripts/UIManager.cs
@@ -9,6 +9,8 @@ public class UIManager : MonoBehaviour
     #region Menu
     [SerializeField] private GameObject menuScreen;
     [SerializeField] private GameObject soundSettingsScreen;
+    [SerializeField] private GameObject resolutionSettingsScreen;
+    [SerializeField] private GameObject mouseSensitivitySettingsScreen;
     #endregion
 
     private void Awake() 
@@ -19,6 +21,8 @@ public class UIManager : MonoBehaviour
 
     private void Update() 
     {
+        //Menu Screen에서 esc 누르면 종료하시겠습니까? 창이 열리면 좋겠음
+        //그리고 다른 화면에서 esc창을 누르면 이전화면으로 가는걸로 바뀌면 좋겠음
         if(Input.GetKeyDown(KeyCode.Backspace) || Input.GetMouseButtonDown(1)) ShowMenuScreen();
         if(Input.GetKeyDown(KeyCode.Escape)) Application.Quit();
     }
@@ -33,6 +37,8 @@ public class UIManager : MonoBehaviour
         if(_screen.activeSelf) return;
         menuScreen.SetActive(false);
         soundSettingsScreen.SetActive(false);
+        resolutionSettingsScreen.SetActive(false);
+        mouseSensitivitySettingsScreen.SetActive(false);
 
         _screen.SetActive(true);
     }
@@ -45,5 +51,15 @@ public class UIManager : MonoBehaviour
     public void ShowSoundSettingsScreen()
     {
         ShowScreen(soundSettingsScreen);
+    }
+
+    public void ShowResolutionSettingsScreen()
+    {
+
+    }
+
+    public void ShowMouseSensitivitySettingsScreen()
+    {
+
     }
 }
