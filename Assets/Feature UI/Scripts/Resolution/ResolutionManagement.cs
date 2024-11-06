58423a902b2adff7add5ee37dc257a0c3137bd38
진성혁 <wtg0516@naver.com>
2024-11-06 21:18:40 +0900

    화면 비율 코드 임시 커밋

--- Assets/Feature UI/Scripts/Resolution/ResolutionManagement.cs
+++ Assets/Feature UI/Scripts/Resolution/ResolutionManagement.cs
@@ -9,48 +9,84 @@ public class ResolutionManagement : MonoBehaviour
     [SerializeField] Camera cam;
     [SerializeField] Text screenText;
 
+    int temp = 0;
+    int nowWidth = 0;
+    int nowHeight = 0;
+
+    private void Awake()
+    {
+        //cam.clearFlags = CameraClearFlags.SolidColor;
+        //cam.backgroundColor = Color.black;
+        //GL.Clear(true, true, Color.black);
+        //ChangeResolution(32, 9);
+        //Hi();
+    }
+
     private void Update()
     {
 
-        screenText.text = Screen.width + "";
+        //screenText.text = Screen.width + "";
+        screenText.text = nowWidth + " : " + nowHeight;
+
+        if (Input.GetKeyDown(KeyCode.N))
+        {
+            //GL.Clear(true, true, Color.black);
+            //print("black");
+            Hi();
+        }
+        else if (Input.GetKeyDown(KeyCode.M))
+        {
+            Bye();
+        }
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
             print("눌림");
-            switch (Screen.width)
+
+            switch (temp)
             {
-                case 3840:
-                    print("2560, 1440");
-                    Screen.SetResolution(2560, 1440, true);
-                    RescaleCamera(2560, 1440);
-                    //ChangeResolution(2560, 1440);
+                case 0:
+                    print("눌림1");
+                    temp++;
+                    print("눌림2");
+                    RescaleCamera(4, 3);
+                    print("눌림3");
                     break;
-                case 2560:
-                    print("1920, 1080");
-                    Screen.SetResolution(1920, 1080, true);
-                    RescaleCamera(1920, 1080);
-                    //ChangeResolution(1920, 1080);
+
+                case 1:
+                    temp++;
+                    RescaleCamera(16, 10);
                     break;
-                case 1920:
-                    print("1280, 720");
-                    Screen.SetResolution(1280, 720, true);
-                    RescaleCamera(1280, 720);
-                    //ChangeResolution(1280, 720);
+
+                case 2:
+                    temp++;
+                    RescaleCamera(16, 9);
                     break;
-                case 1280:
-                    print("720, 480");
-                    Screen.SetResolution(720, 480, true);
-                    RescaleCamera(720, 480);
-                    //ChangeResolution(720, 480);
+
+                case 3:
+                    temp++;
+                    RescaleCamera(21, 9);
                     break;
-                case 720:
-                    print("3840, 2160");
-                    Screen.SetResolution(3840, 2160, true);
-                    RescaleCamera(3840, 2160);
-                    //ChangeResolution(3840, 2160);
+
+                case 4:
+                    temp = 0;
+                    RescaleCamera(32, 9);
                     break;
             }
+
         }
+    }
+
+    private void Hi()
+    {
+        Screen.SetResolution(1440, 1080, true);
+        print("dd");
+    }
+
+    private void Bye()
+    {
+        Screen.SetResolution(1920, 1080, true);
+        print("dd");
     }
 
     //레터박스 완전 검은색으로 나오게 함
@@ -58,8 +94,11 @@ public class ResolutionManagement : MonoBehaviour
 
     public void ChangeResolution(float width, float height)
     {
+        nowWidth = (int)width;
+        nowHeight = (int)height;
         //cam = GetComponent<Camera>();
-        Rect rect = cam.rect;
+        //Rect rect = cam.rect;
+        Rect rect = new Rect(0, 0, 1, 1);
         float scaleheight = ((float)Screen.width / Screen.height) / ((float)width / height); // (가로 / 세로)
         float scalewidth = 1f / scaleheight;
         if (scaleheight < 1)
@@ -73,7 +112,11 @@ public class ResolutionManagement : MonoBehaviour
             rect.x = (1f - scalewidth) / 2f;
         }
         cam.rect = rect;
+
+        print(cam.rect);
     }
+
+    //void OnPreCull() => GL.Clear(true, true, Color.black);
 
 
     private int ScreenSizeX = 0;
@@ -82,7 +125,13 @@ public class ResolutionManagement : MonoBehaviour
     private void RescaleCamera(float width, float height)
     {
 
-        if (Screen.width == ScreenSizeX && Screen.height == ScreenSizeY) return;
+        //if (Screen.width == ScreenSizeX && Screen.height == ScreenSizeY) return;
+        //Camera cam2 = Camera.main;
+        //cam2.Render();  // 카메라가 현재 프레임을 다시 렌더링하게 함
+        GL.Clear(true, true, Color.black);  // 화면을 검은색으로 지움
+        print("asdfafdd");
+        nowWidth = (int)width;
+        nowHeight = (int)height;
 
         //float targetaspect = 16.0f / 9.0f;
         float targetaspect = width / height;
@@ -94,9 +143,11 @@ public class ResolutionManagement : MonoBehaviour
         {
             Rect rect = cam.rect;
 
-            rect.width = 1.0f;
+            //rect.width = 1.0f;
+            rect.width = scaleheight;
             rect.height = scaleheight;
-            rect.x = 0;
+            //rect.x = 0;
+            rect.x = (1.0f - scaleheight) / 2.0f;
             rect.y = (1.0f - scaleheight) / 2.0f;
 
             cam.rect = rect;
@@ -108,9 +159,11 @@ public class ResolutionManagement : MonoBehaviour
             Rect rect = cam.rect;
 
             rect.width = scalewidth;
-            rect.height = 1.0f;
+            //rect.height = 1.0f;
+            rect.height = scalewidth;
             rect.x = (1.0f - scalewidth) / 2.0f;
-            rect.y = 0;
+            //rect.y = 0;
+            rect.y = (1.0f - scalewidth) / 2.0f;
 
             cam.rect = rect;
         }
@@ -126,7 +179,7 @@ public class ResolutionManagement : MonoBehaviour
         Rect nr = new Rect(0, 0, 1, 1);
 
         Camera.main.rect = nr;
-        GL.Clear(true, true, Color.black);
+        GL.Clear(true, true, Color.red);
 
         Camera.main.rect = wp;
 
