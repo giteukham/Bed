7f80d7830644bfd75ddcaaa18168c870e5c762cd
진성혁 <wtg0516@naver.com>
2024-11-08 16:38:20 +0900

    해상도 코드 변경

--- Assets/Feature UI/Scripts/Resolution/ResolutionManagement.cs
+++ Assets/Feature UI/Scripts/Resolution/ResolutionManagement.cs
@@ -13,6 +13,8 @@ public class ResolutionManagement : MonoBehaviour
     int nowWidth = 0;
     int nowHeight = 0;
 
+    const float CRITERIA_NUM = 16 / 9;
+
     private void Awake()
     {
         //cam.clearFlags = CameraClearFlags.SolidColor;
@@ -49,32 +51,93 @@ public class ResolutionManagement : MonoBehaviour
                     print("눌림1");
                     temp++;
                     print("눌림2");
-                    RescaleCamera(4, 3);
+                    TestResolution(4, 3);
                     print("눌림3");
                     break;
 
                 case 1:
                     temp++;
-                    RescaleCamera(16, 10);
+                    TestResolution(16, 10);
                     break;
 
                 case 2:
                     temp++;
-                    RescaleCamera(16, 9);
+                    TestResolution(16, 9);
                     break;
 
                 case 3:
                     temp++;
-                    RescaleCamera(21, 9);
+                    TestResolution(21, 9);
                     break;
 
                 case 4:
                     temp = 0;
-                    RescaleCamera(32, 9);
+                    TestResolution(32, 9);
                     break;
             }
 
         }
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="width">'목표' 가로비율로 명명함</param>
+    /// <param name="height">'목표' 세로비율로 명명함</param>
+    private void TestResolution(float width, float height)
+    {
+        GL.Clear(true, true, Color.black);  // 화면을 검은색으로 지움
+
+        //16 / 9값과 비교하여 16:9 화면에서 세로길이 혹은 가로길이 중에서
+        //어느 길이가 더 긴지 알아내는 판별용 변수
+        float checkedValue = width / height;
+        Rect rect = new Rect(0, 0, 1, 1);
+        float temp1 = 0;
+        //세로 비율을 9로 통일 후 변경될 목표 가로 비율
+        float temp2 = 0;
+
+        //목표 비율이 16 : 9일때
+        if (checkedValue == CRITERIA_NUM)
+        {
+            cam.rect = rect;
+            print("16 : 9");
+            return;
+        }
+        //목표 비율의 '가로 비율'이 16 : 9보다 클때
+        else if (checkedValue > CRITERIA_NUM)
+        {
+            //목표 비율 * X = 9가 성립될 때의 X값을 구함
+            temp1 = 9 / height;
+            //미지수 X와 '목표' 비율의 가로를 곱하여 '최종' 가로비율을 구함
+            temp2 = temp1 * width;
+
+            //수정된 '최종'비율은 다음과 같음  (temp2 : 9)
+
+            //16 / '최종' 가로비율의 값을 rect.width에 대입
+            rect.width = 16 / temp2;
+
+            //1 - rect.width를 한 뒤 2를 나눈 값을 rect.x에 대입(밀린 화면 중앙으로 이동)
+            rect.x = (1 - rect.width) / 2;
+        }
+        //목표 비율의 '세로 비율'이 16 : 9보다 클때
+        else if (checkedValue < CRITERIA_NUM)
+        {
+            //목표 비율 * X = 9가 성립될 때 X값을 구함
+            temp1 = 9 / height;
+            //미지수 X와 '목표' 비율의 가로를 곱하여 '최종' 가로비율을 구함
+            temp2 = temp1 * width;
+
+            //수정된 '최종'비율은 다음과 같음  (temp2 : 9)
+
+            //'최종' 가로비율 / 16의 값을 rect.height에 대입
+            rect.height = temp2 / 16;
+
+            //1 - rect.height를 한 뒤 2를 나눈 값을 rect.y에 대입(밀린 화면 중앙으로 이동)
+            rect.y = (1 - rect.height) / 2;
+        }
+
+        //카메라에 최종적용
+        cam.rect = rect;
     }
 
     private void Hi()
@@ -143,11 +206,9 @@ public class ResolutionManagement : MonoBehaviour
         {
             Rect rect = cam.rect;
 
-            //rect.width = 1.0f;
-            rect.width = scaleheight;
+            rect.width = 1.0f;
             rect.height = scaleheight;
-            //rect.x = 0;
-            rect.x = (1.0f - scaleheight) / 2.0f;
+            rect.x = 0;
             rect.y = (1.0f - scaleheight) / 2.0f;
 
             cam.rect = rect;
@@ -159,11 +220,9 @@ public class ResolutionManagement : MonoBehaviour
             Rect rect = cam.rect;
 
             rect.width = scalewidth;
-            //rect.height = 1.0f;
-            rect.height = scalewidth;
+            rect.height = 1.0f;
             rect.x = (1.0f - scalewidth) / 2.0f;
-            //rect.y = 0;
-            rect.y = (1.0f - scalewidth) / 2.0f;
+            rect.y = 0;
 
             cam.rect = rect;
         }
