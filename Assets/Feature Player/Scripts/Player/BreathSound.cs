75fc4615b7f5ecdb3674bfada51b7a194a04542f
muryung <62589813+muryung@users.noreply.github.com>
2025-02-04 00:00:12 +0900

    Breath 애니메이션 업데이트

--- Assets/Feature Player/Scripts/Player/BreathSound.cs
+++ Assets/Feature Player/Scripts/Player/BreathSound.cs
@@ -1,9 +1,22 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class BreathSound : MonoBehaviour
 {
+    private Animator headAnimator;
+    
+    private void Start()
+    {
+        headAnimator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        StopBreath(PlayerConstant.isMovingState);
+    }
+
     public void InhaleSound()
     {
         AudioManager.Instance.PlayOneShot(AudioManager.Instance.inhale, transform.position);
@@ -12,5 +25,20 @@ public class BreathSound : MonoBehaviour
     public void ExhaleSound()
     {
         AudioManager.Instance.PlayOneShot(AudioManager.Instance.exhale, transform.position);
+    }
+
+    public void StopBreath(bool isStop)
+    {
+        headAnimator.SetBool("isStop", isStop);
+    }
+    
+    public void ToInhale()
+    {
+        headAnimator.SetTrigger("toInhale");
+    }
+    
+    public void ToExhale()
+    {
+        headAnimator.SetTrigger("toExhale");
     }
 }
