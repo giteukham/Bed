c10f171513b981557c3fb196f11157a73ed26454
진성혁 <wtg0516@naver.com>
2025-02-12 19:21:58 +0900

    이펙트 인터페이스 추가, 이펙트 활성, 비활성화 효과 추가

--- Assets/Feature UI/Scripts/Tutorial/Blink.cs
+++ Assets/Feature UI/Scripts/Tutorial/Blink.cs
@@ -3,31 +3,67 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Blink : MonoBehaviour
+public class Blink : MonoBehaviour, ITutorialEffect
 {
+    private Sequence sequence;
     private CanvasGroup canvasGroup;
-    [SerializeField] private float fadeInDuration = 0.1f;  // 페이드 인
-    [SerializeField] private float fadeOutDuration = 0.3f;  // 페이드 아웃
+    private CanvasGroup parentCanvasGroup;
 
-    private void Start()
+    [SerializeField] private GameObject parent;
+    [SerializeField] private float runTimeFadeInDuration = 0.1f;  // 페이드 인
+    [SerializeField] private float runTimeFadeOutDuration = 0.3f;  // 페이드 아웃
+    [SerializeField] private float fadeInDuration = 0.4f;
+    [SerializeField] private float fadeOutDuration = 0.4f;
+
+    private void Awake()
     {
         canvasGroup = GetComponent<CanvasGroup>();
+        parentCanvasGroup = parent.GetComponent<CanvasGroup>();
+    }
 
-        //처음에 안보이게
-        canvasGroup.DOFade(0, 0.01f);
+    private void OnEnable()
+    {
+        //모든 이펙트 투명화로 시작
+        canvasGroup.alpha = 0;
+        parentCanvasGroup.alpha = 0;
 
-        PlayEffect();
+        //이펙트 전체가 천천히 나타남
+        parent.GetComponent<CanvasGroup>().DOFade(1, fadeInDuration);
+
+        if (sequence != null)
+        {
+            //시퀀스 재시작
+            sequence.Restart();
+        }
+        else
+        {
+            //이펙트 시퀀스에 등록
+            PlayEffect();
+        }
+    }
+
+    public void OffEffect()
+    {
+        //이펙트 전체가 천천히 사라짐
+        parentCanvasGroup.DOFade(0, fadeInDuration).OnComplete(() =>
+        {
+            //이펙트 일시정지
+            sequence.Pause();
+            //이펙트 비활성화
+            gameObject.SetActive(false);
+        }
+        );
     }
 
     private void PlayEffect()
     {
-        Sequence sequence = DOTween.Sequence();
+        sequence = DOTween.Sequence();
 
         //서서히 나타남
-        sequence.Append(canvasGroup.DOFade(1, fadeInDuration));
+        sequence.Append(canvasGroup.DOFade(1, runTimeFadeInDuration));
 
         //서서히 사라짐
-        sequence.Append(canvasGroup.DOFade(0, fadeOutDuration));
+        sequence.Append(canvasGroup.DOFade(0, runTimeFadeOutDuration));
 
         //무한 반복 설정
         sequence.SetLoops(-1);
--- Assets/Feature UI/Scripts/Tutorial/EyeOpen.cs
+++ Assets/Feature UI/Scripts/Tutorial/EyeOpen.cs
@@ -3,15 +3,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BothClick : MonoBehaviour, ITutorialEffect
+public class EyeOpen : MonoBehaviour, ITutorialEffect
 {
+
     private Sequence sequence;
     private CanvasGroup canvasGroup;
     private CanvasGroup parentCanvasGroup;
 
     [SerializeField] private GameObject parent;
-    [SerializeField] private float runTimeFadeInDuration = 0.1f;  // 페이드 인
-    [SerializeField] private float runTimeFadeOutDuration = 0.3f;  // 페이드 아웃
+    [SerializeField] private float runTimeFadeDuration = 0.2f;  // 페이드 인/아웃 시간
+    [SerializeField] private float moveDuration = 0.6f; // 이동 시간
+    [SerializeField] private float moveOffset = 10f;  // 위로 올라가는 거리
     [SerializeField] private float fadeInDuration = 0.4f;
     [SerializeField] private float fadeOutDuration = 0.4f;
 
@@ -60,10 +62,17 @@ public class BothClick : MonoBehaviour, ITutorialEffect
         sequence = DOTween.Sequence();
 
         //서서히 나타남
-        sequence.Append(canvasGroup.DOFade(1, runTimeFadeInDuration));
+        sequence.Append(canvasGroup.DOFade(1, runTimeFadeDuration));
+
+        //살짝 위로 이동 (RectTransform의 anchoredPosition 사용)
+        sequence.Append(GetComponent<RectTransform>().DOAnchorPosY(GetComponent<RectTransform>().anchoredPosition.y + moveOffset, moveDuration));
 
         //서서히 사라짐
-        sequence.Append(canvasGroup.DOFade(0, runTimeFadeOutDuration));
+        sequence.Append(canvasGroup.DOFade(0, runTimeFadeDuration));
+
+        //휠 위치 빠르게 원위치
+        //sequence.Append(GetComponent<RectTransform>().DOAnchorPosY(GetComponent<RectTransform>().anchoredPosition.y, 0.01f));
+        sequence.OnComplete(() => GetComponent<RectTransform>().anchoredPosition = Vector2.zero);
 
         //무한 반복 설정
         sequence.SetLoops(-1);
