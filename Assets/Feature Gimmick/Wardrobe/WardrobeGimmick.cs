62090854d4bbd409bcba2f2e04f350718f96f62f
진성혁 <wtg0516@naver.com>
2025-02-18 14:50:00 +0900

    옷장 기믹 완성

--- Assets/Feature Gimmick/Wardrobe/WardrobeGimmick.cs
+++ Assets/Feature Gimmick/Wardrobe/WardrobeGimmick.cs
@@ -18,9 +18,10 @@ public class WardrobeGimmick : Gimmick
     #endregion
 
     #region Variables
-    [SerializeField] private GameObject cat;
     [SerializeField] private GameObject wardrobe;
     [SerializeField] private GameObject rightDoor;
+    [SerializeField] private GameObject cat;
+    [SerializeField] private GameObject eyesAndDark;
     private Vector3 catPosition;
     private Vector3 wardrobePosition;
     private Quaternion wardrobeRotation;
@@ -32,23 +33,13 @@ public class WardrobeGimmick : Gimmick
         wardrobe.transform.SetParent(null);
         wardrobePosition = wardrobe.transform.localPosition;
         wardrobeRotation = wardrobe.transform.localRotation;
-
-    }
-
-    IEnumerator TestCode()
-    {
-        yield return new WaitForSeconds(5);
-        wardrobe.transform.DOShakePosition(1, 0.02f, 2, 90, false, false).OnComplete(() => wardrobe.transform.localPosition = wardrobePosition);
+        cat.SetActive(false);
+        eyesAndDark.SetActive(false);
     }
 
     private void Update()
     {
         timeLimit += Time.deltaTime;
-        if (Input.GetKeyDown(KeyCode.T))
-        {
-            wardrobe.transform.DOShakePosition(1, 0.02f, 5, 90, false, false, ShakeRandomnessMode.Full).OnComplete(() => wardrobe.transform.localPosition = wardrobePosition);
-            wardrobe.transform.DOShakeRotation(1, 0.05f, 5, 90, false, ShakeRandomnessMode.Full).OnComplete(() => wardrobe.transform.localRotation = wardrobeRotation);
-        }
     }
 
     public override void Activate()
@@ -60,6 +51,8 @@ public class WardrobeGimmick : Gimmick
     public override void Deactivate()
     {
         base.Deactivate();
+        cat.SetActive(false);
+        eyesAndDark.SetActive(false);
         gameObject.SetActive(false);
     }
 
@@ -70,8 +63,8 @@ public class WardrobeGimmick : Gimmick
 
     public override void UpdateProbability()
     {
-        //probability = (PlayerConstant.LeftFrontLookCAT) + (PlayerConstant.LeftFrontLookLAT / 4);
-        probability = 100;
+        probability = (PlayerConstant.LeftFrontLookCAT) + (PlayerConstant.LeftFrontLookLAT / 4);
+        //probability = 100;
     }
 
     private IEnumerator MainCode()
@@ -79,7 +72,7 @@ public class WardrobeGimmick : Gimmick
         print("메인코드 실행");
         //삐그덕 거리는 소리 들리면서 옷장 흔들림
         AudioManager.Instance.PlaySound(AudioManager.Instance.wardrobeHinges, transform.position);
-        wardrobe.transform.DOShakePosition(1, 1, 2, 90, false, false, ShakeRandomnessMode.Full).OnComplete(() => wardrobe.transform.localPosition = wardrobePosition);
+        wardrobe.transform.DOShakePosition(1, 0.02f, 5, 90, false, false, ShakeRandomnessMode.Full).OnComplete(() => wardrobe.transform.localPosition = wardrobePosition);
         wardrobe.transform.DOShakeRotation(1, 0.05f, 5, 90, false, ShakeRandomnessMode.Full).OnComplete(() => wardrobe.transform.localRotation = wardrobeRotation);
 
         float num = PlayerConstant.LeftFrontLookCAT;
@@ -93,15 +86,25 @@ public class WardrobeGimmick : Gimmick
         }
         else
         {
-            //문이 서서히 열림
-            rightDoor.transform.DOLocalRotateQuaternion(Quaternion.Euler(0, -15, 0) * rightDoor.transform.localRotation, 2f);
-
             //이후 확률에 따라 진행
             int randomNum = Random.Range(1, 101);
+            if (randomNum <= 70)
+            {
+                cat.SetActive(true);
+            }
+            else
+            {
+                eyesAndDark.SetActive(true);
+            }
+
+            //문이 서서히 열림
+            rightDoor.transform.DOLocalRotateQuaternion(Quaternion.Euler(0, -25, 0) * rightDoor.transform.localRotation, 2f);
+            
             //70퍼 확률로 고양이가 빼꼼 나와서 그냥 움
             if (randomNum <= 70)
             {
                 print("고양이 기믹 실행");
+                cat.SetActive(true);
                 //소리냄
                 AudioManager.Instance.PlaySound(AudioManager.Instance.wardrobeCat, cat.transform.position);
                 //앞으로 살짝 나옴
@@ -114,27 +117,22 @@ public class WardrobeGimmick : Gimmick
                 //잠시 대기
                 yield return new WaitForSeconds(10);
                 //다시 들어감
-                cat.transform.DOLocalMove(catPosition, 2f).OnComplete(
-                    () =>
-                    {
-                        cat.transform.localPosition = new Vector3(-0.46f, 0.28f, -0.35f);
-                        Deactivate();
-                    }
-                    );
+                cat.transform.DOLocalMove(catPosition, 2f).OnComplete(() => cat.transform.localPosition = new Vector3(-0.46f, 0.28f, -0.35f));
 
             }
             //30퍼 확률로 장롱안에서 하얀색 안광만 보임
             else
             {
+                print("안광 기믹 실행");
+                eyesAndDark.SetActive(true);
                 //잠시 대기
                 yield return new WaitForSeconds(5);
-                print("안광 기믹 실행");
                 GaugeController.Instance.SetGuage(GaugeController.GaugeTypes.Stress, +10);
                 GaugeController.Instance.SetGuage(GaugeController.GaugeTypes.Fear, +10);
             }
 
             //문 닫히는 코드 추가후 Deactivate();
-            rightDoor.transform.DOLocalRotateQuaternion(Quaternion.Euler(0, 15, 0) * rightDoor.transform.localRotation, 2f).OnComplete(() => Deactivate());
+            rightDoor.transform.DOLocalRotateQuaternion(Quaternion.Euler(0, 25, 0) * rightDoor.transform.localRotation, 2f).OnComplete(() => Deactivate());
         }
     }
 }
