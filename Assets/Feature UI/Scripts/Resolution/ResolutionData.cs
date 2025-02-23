5cc3dda6cc661294929b29d0a8ef4f20d63a9cee
KwonSungWoo <ppoppo831@g.yju.ac.kr>
2025-02-23 13:03:33 +0900

    UI 배치 및 색 변경

--- Assets/Feature UI/Scripts/Resolution/ResolutionData.cs
+++ Assets/Feature UI/Scripts/Resolution/ResolutionData.cs
@@ -17,7 +17,7 @@ public struct ResolutionData
     
     public bool Equals(ResolutionData other)
     {
-        Debug.Log($"실제 값: {width}, {height}, {frameRate}, {isWindowedScreen}, 준비 값: {other.width}, {other.height}, {other.frameRate}, {other.isWindowedScreen}");
+        //Debug.Log($"실제 값: {width}, {height}, {frameRate}, {isWindowedScreen}, 준비 값: {other.width}, {other.height}, {other.frameRate}, {other.isWindowedScreen}");
         return width == other.width && height == other.height && frameRate == other.frameRate && isWindowedScreen == other.isWindowedScreen;
     }
 }
