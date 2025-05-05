a7b45990a81f880508b542db6096dd0f7a799f2b
muryung <62589813+muryung@users.noreply.github.com>
2025-05-05 15:43:46 +0900

    Mirror 기믹 추가

---
+++ Assets/Feature Gimmick/New Gimmicks/MirrorGimmick.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using AbstractGimmick;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class MirrorGimmick : Gimmick
+{
+    public override GimmickType type { get; protected set; }
+    public override float probability { get; set; }
+    public override List<Gimmick> ExclusionGimmickList { get; set; }
+    
+    [SerializeField]
+    private GameObject monsterObj;
+    private Coroutine mirrorCoroutine;
+
+    private void Awake()
+    {
+        if (monsterObj.activeSelf == true) monsterObj.SetActive(false);
+        
+        mirrorCoroutine = StartCoroutine(PlayMirrorGimmick());
+
+    }
+
+    public override void UpdateProbability()
+    {
+    }
+
+    public override void Initialize()
+    {
+    }
+
+    public override void Activate()
+    {
+        base.Activate();
+        mirrorCoroutine = StartCoroutine(PlayMirrorGimmick());
+    }
+
+    private IEnumerator PlayMirrorGimmick()
+    {
+        yield return new WaitUntil(() => isDetected);
+        monsterObj.SetActive(true);
+        
+        yield return new WaitUntil(() => !isDetected);
+        monsterObj.SetActive(false);
+    }
+
+    public override void Deactivate()
+    {
+        base.Deactivate();
+        
+        if (mirrorCoroutine != null)
+        {
+            StopCoroutine(mirrorCoroutine);
+            mirrorCoroutine = null;
+        }
+    }
+}
