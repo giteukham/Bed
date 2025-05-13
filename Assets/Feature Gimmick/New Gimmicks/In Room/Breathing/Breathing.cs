604efed427d4928bdfc0a350fed060f2c0581733
muryung <62589813+muryung@users.noreply.github.com>
2025-05-13 11:45:58 +0900

    Breathing 기믹 위치 조정 추가

--- Assets/Feature Gimmick/New Gimmicks/In Room/Breathing/Breathing.cs
+++ Assets/Feature Gimmick/New Gimmicks/In Room/Breathing/Breathing.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using AbstractGimmick;
 using FMODUnity;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class Breathing : SoundOnlyGimmick
 {
@@ -11,6 +12,14 @@ public class Breathing : SoundOnlyGimmick
     public override float probability { get; set; }
     public override List<Gimmick> ExclusionGimmickList { get; set; }
     protected override EventReference soundEvent { get; set; }
+
+    [SerializeField]
+    private Transform leftEar;
+    
+    [SerializeField]
+    private Transform rightEar;
+
+    private bool isActive = false;
 
     public override void UpdateProbability()
     {
@@ -22,5 +31,44 @@ public class Breathing : SoundOnlyGimmick
     private void Start()
     {
         soundEvent = AudioManager.Instance.breathingInRoom;
+    }
+
+    public override void Activate()
+    {
+        base.Activate();
+        StartCoroutine(ActivateBreathing());
+        // TODO: AudioManager.Instance.PlaySound(soundEvent, transform.position);
+    }
+    
+    private IEnumerator ActivateBreathing()
+    {
+        isActive = true;
+
+        while (true)
+        {
+            if (isActive == false) yield break;
+            
+            if (PlayerConstant.isLeftState)
+            {
+                AudioManager.Instance.SetPosition(soundEvent, rightEar.position);
+            }
+            else if (PlayerConstant.isRightState)
+            {
+                AudioManager.Instance.SetPosition(soundEvent, leftEar.position);
+            }
+            else if (PlayerConstant.isMiddleState)
+            {
+                var random = new Transform[2] {leftEar, rightEar};
+                AudioManager.Instance.SetPosition(soundEvent, random[Random.Range(0, random.Length - 1)].position);
+            }
+
+            yield return null;
+        }
+    }
+
+    public override void Deactivate()
+    {
+        base.Deactivate();
+        isActive = false;
     }
 }
